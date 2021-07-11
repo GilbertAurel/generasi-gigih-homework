401e6ff7a7f7e63472afa0a2a6c3cb485703336b
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-07-11 10:55:59 +0700

    feat: add spotify login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { css, jsx } from "@emotion/react";
-import Home from "components/home";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import HomePage from "components/home";
+import LoginPage from "components/login";
 
 function App() {
+  // const [tracks, setTracks] = useState([]);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://api.spotify.com/v1/tracks", {
+  //       headers: {
+  //         Authorization: `Bearer ${process.env.REACT_APP_SPOTIFY_ID}`,
+  //       },
+  //     })
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.log(err));
+  // }, []);
+
   return (
     <div className="App">
-      <Home />
+      <LoginPage />
     </div>
   );
 }
