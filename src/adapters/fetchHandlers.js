868d1570c9791eee6454b0e31ae56f48fd92cb38
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-07-27 22:29:22 +0700

    fix: move redundant business logic to actions

--- src/adapters/fetchHandlers.js
+++ src/adapters/fetchHandlers.js
@@ -1,30 +1,13 @@
 import axios from "axios";
-import {
-  SPOTIFY_SEARCH_URL,
-  SPOTIFY_USER_DATA_URL,
-  GIPHY_SEARCH_URL,
-  SPOTIFY_FETCH_PLAYLIST_URL,
-} from "constants/urls";
+import { SPOTIFY_SEARCH_URL } from "constants/urls";
 
-function fetchData(url, config) {
+export default function fetchData(url, config = {}) {
   return axios
     .get(url, config)
     .then((response) => response.data)
     .catch((error) => console.log(error));
 }
 
-export function GIPHY_FETCH_SEARCH(config) {
-  return fetchData(GIPHY_SEARCH_URL, config);
-}
-
 export function SPOTIFY_FETCH_SEARCH(config) {
   return fetchData(SPOTIFY_SEARCH_URL, config);
-}
-
-export function SPOTIFY_FETCH_USER_DATA(config) {
-  return fetchData(SPOTIFY_USER_DATA_URL, config);
-}
-
-export function SPOTIFY_FETCH_PLAYLIST(config) {
-  return fetchData(SPOTIFY_FETCH_PLAYLIST_URL, config);
 }
