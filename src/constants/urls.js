7bd476709b1118215d9c41086cf7aad76a2bf7a5
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-07-27 22:30:08 +0700

    feat: add new url for getting spotify current track

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,6 +1,12 @@
 export const DEV_URL = encodeURIComponent("http://localhost:3000");
 
-export const SPOTIFY_AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_ID}&response_type=token&redirect_uri=${DEV_URL}&scope=playlist-modify-private%20playlist-read-private`;
+const scope = [
+  "user-read-currently-playing",
+  "playlist-modify-private",
+  "playlist-read-private",
+].join("%20");
+
+export const SPOTIFY_AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_ID}&response_type=token&redirect_uri=${DEV_URL}&scope=${scope}`;
 
 export const SPOTIFY_AUTH_LOGOUT = `https://accounts.spotify.com/en/logout`;
 
@@ -10,8 +16,14 @@ export const SPOTIFY_SEARCH_URL = "https://api.spotify.com/v1/search";
 
 export const SPOTIFY_USER_DATA_URL = "https://api.spotify.com/v1/me";
 
-export const SPOTIFY_CREATE_PLAYLIST_URL = (id) =>
-  `https://api.spotify.com/v1/users/${id}/playlists`;
+export const SPOTIFY_CREATE_PLAYLIST_URL = (user_id) =>
+  `https://api.spotify.com/v1/users/${user_id}/playlists`;
 
 export const SPOTIFY_FETCH_PLAYLIST_URL =
   "https://api.spotify.com/v1/me/playlists";
+
+export const SPOTIFY_FETCH_PLAYLIST_TRACK_URL = (playlist_id) =>
+  `https://api.spotify.com/v1/playlists/${playlist_id}/tracks`;
+
+export const SPOTIFY_FETCH_CURRENTLY_PLAYING_URL =
+  "https://api.spotify.com/v1/me/player/currently-playing";
